2db60031f923a9d9ddd4a004f05d326e423a1211
AnitBishwas <anit.biswas@swissbeauty.in>
2025-11-17 00:24:07 +0530

    Test run

--- controllers/index.js
+++ controllers/index.js
@@ -13,8 +13,9 @@ const updateSkusDataInGoogleSheet = async () => {
     // current sheet
     const doc = new GoogleSpreadsheet(process.env.GOOGLE_SHEET_ID, googleToken);
     await doc.loadInfo();
-    console.log(doc)
     const sheet = await doc.sheetsById[process.env.GOOGLE_SHEET_INVENTORY_ID];
+    console.log(sheet);
+    
     await sheet.clear();
     const { variantLevelData, productLevelData } = await generateProductsData();
     console.log('👉 updating inventory data for variants');
